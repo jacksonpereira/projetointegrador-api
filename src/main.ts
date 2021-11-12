f6a9bc132f53d3918415b672f170bb6c9ef2f6e1
Jackson Silva <jacksonga@hotmail.com>
2021-11-11 23:09:59 -0300

    Configurando porta e adicionando segurança

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+var timeout = require('connect-timeout');
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(timeout('20s'));
+  app.use(haltOnTimedout);
+  app.use(helmet());
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 1000, // limit each IP to 1000 requests per windowMs
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
